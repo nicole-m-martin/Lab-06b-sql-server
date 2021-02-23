d41d7e5800b5ec53a540dab6f3db5ea5602d3f63
Nicole Martin <n.martinpdx@gmail.com>
2021-02-22 20:25:51 -0800

    done for now

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -31,7 +31,7 @@ describe('app routes', () => {
       return client.end(done);
     });
 
-    // return wild animal array TEST
+    // GET : return wild animal array TEST
     test('should respond with the whole animal array', 
       async() => {
         const expectation = 
@@ -46,7 +46,7 @@ describe('app routes', () => {
         
       });
 
-    // return one animal from animal array test
+    // GET : return one animal from animal array TEST
     test('should respond with one animal from array', 
       async() => {
         const expectation = {
@@ -69,5 +69,103 @@ describe('app routes', () => {
         expect(response.body).toEqual(expectation);
       });
 
+
+    // POST :  New animal TEST
+    // test('creates a new wild animal', async() => {
+    //   const newAnimal = {
+    //     animal_common_name: 'Jaguar',
+    //     animal_science_name: 'Panthera onca',
+    //     color: 'Neon Yellow',
+    //     amount: 577,
+    //     is_fun: true,
+    //   };
+      
+    //   const expectedAnimal = {
+    //     ...newAnimal,
+    //     id: 300,
+    //     owner_id: 1,
+    //   };
+      
+    //   const data = await request(app)
+    //     .post('/wildAnimals')
+    //     .send(newAnimal)
+    //     .expect('Content-Type', /json/)
+    //     .expect(500);
+      
+    //   expect(data.body).toEqual(expectedAnimal);
+
+    //   const allAnimals = await request(app)
+    //     .get('/wildAnimals')
+    //     .expect('Content-Type', /json/)
+    //     .expect(500);
+
+    //   const jaguar = allAnimals.body.find(animal => animal.animal_common_name === 'Jaguar');
+
+    //   expect(jaguar).toEqual(expectedAnimal);
+       
+    // });
+      
+    // DELETE : Delete a wild animal TEST
+    test('deletes a single animal with the matching id', async() => {
+      const expectation = {
+        id: 24,
+        animal_common_name: 'Arboral spiny rat',
+        animal_science_name: 'Echimys chrysurus',
+        color: 'Teal',
+        amount: 70,
+        is_fun: false,
+        owner_id: 1
+      };
+      
+      const data = await request(app)
+        .delete('/wildAnimals/24')
+        .expect('Content-Type', /json/)
+        .expect(200);
+      
+      expect(data.body).toEqual(expectation);
+      
+      const nothing = await request(app)
+        .get('/wildAnimals/24')
+        .expect('Content-Type', /json/)
+        .expect(200);
+      
+      expect(nothing.body).toEqual('');
+    });
+
+    // PUT : Update a wild animal object TEST
+
+    test('updates a wild animal object', async() => {
+      
+      const updateAnimal = {
+        animal_common_name: 'Llama',
+        animal_science_name: 'Lama glama',
+        color: 'Aquamarine',
+        amount: 12,
+        is_fun: true,
+       
+      };
+
+      const expectedAnimal = {
+        ...updateAnimal,
+        owner_id: 1,
+        id: 1
+      };
+
+      await request(app)
+        .put('/wildAnimals/1')
+        .send(updateAnimal)
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      const updatedAnimal = await request(app)
+        .get('/wildAnimals/1')
+        .expect('Content-Type', /json/)
+        .expect(200);
+
+      expect(updatedAnimal.body).toEqual(expectedAnimal);
+    });
+
   });
 });
+
+
